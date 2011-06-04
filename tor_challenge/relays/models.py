4d6685b0478a2614b1ec003a6d10ec6f47328b7a
Jason Cox <JasonWilburCox@gmail.com>
2011-06-04 15:51:40 -0400

    Linked the model to django

--- tor_challenge/relays/models.py
+++ tor_challenge/relays/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class Relay(models.Model):
     fingerprint = models.CharField(max_length=255)
     username = models.CharField(max_length=255)
-    is_anonymous = models.BooleanField
+    is_anonymous = models.BooleanField()
 
 class RelayDetails(models.Model):
     name = models.CharField(max_length=257) # TODO: figure out if this is a performance issue
     bandwidth = models.CharField(max_length=257)
-    status = models.IntField
+    status = models.IntegerField()
--- tor_challenge/settings.py
+++ tor_challenge/settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': './relays.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -115,6 +115,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'relays'
+
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
