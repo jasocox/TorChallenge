fa196c00911e8fea57935d496f642cf758d6cf71
Jim Yi <jim@jimyi.com>
2011-06-04 18:05:06 -0400

    rename method, add comment

--- RelayParser.py
+++ RelayParser.py
@@ -9,17 +9,20 @@ class RelayParser:
         f.close()
 	return output
 
-    def is_fingerprint_fresh(self, fingerprint):
+    def lookup_fingerprint(self, fingerprint):
         # Perform an HTTP relay search with the fingerprint and scrape the page
 	relay_search_url = "https://metrics.torproject.org/relay-search.html?search=" + fingerprint
 	relay_search_page = urllib2.urlopen(relay_search_url).read().split("\n")
 	for line in relay_search_page:
+	    # Look for the first instance of valid-after, since that will be the most recent metric
 	    if line.find("<tt>valid-after") != -1:
+	        # Parse out the date which is enclosed by an <a> tag
 	        date_start = line.find("\"_blank\">") + 9
 		date_end = line.find("</a>")
 	        date = line[date_start:date_end]
 		parsed_date = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
-		if (time.mktime(parsed_date.timetuple()) + 24 * 60 * 60 > time.mktime(datetime.datetime.now().timetuple())):
+                # Make sure this last metric is within 5 hours of now
+		if (time.mktime(parsed_date.timetuple()) + 5 * 60 * 60 > time.mktime(datetime.datetime.now().timetuple())):
 		    print "fresh: " + fingerprint
 		else:
 		    print "old: " + fingerprint
