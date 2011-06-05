beacb8d514b68800aa8938301f12bf8f49f3075f
Jason Cox <JasonWilburCox@gmail.com>
2011-06-05 14:31:16 -0400

    Added some error detection to the EFF Tor Challenge site (its not very reliable)

--- foo.sh
+++ foo.sh
@@ -1,3 +1,10 @@
 wget --no-check-certificate https://www.eff.org/torchallenge/list/ -O index.html
+#wget --no-check-certificate http://www./torchallenge/list/ -O index.html
+
+if [ $? != 0 ]; then
+    echo "\nERROR: The EFF server is currently down. Try again later"
+    exit -1
+fi
+
 grep view-content-torchallenge index.html > content
-python scrape.py conten > print
+python scrape.py content > prints
